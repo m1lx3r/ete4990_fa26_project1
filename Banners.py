2eabd5a9b04ec995da978a94bfb8b5b23057d0b3
Moises A. Santander <51764308+m1lx3r@users.noreply.github.com>
2026-09-17 17:33:55 -0700

    Update Banners.py

--- Banners.py
+++ Banners.py
@@ -3,61 +3,105 @@
 """ Purpose: Calls classes for other banners: StartScreen, EndScreen, RegularBanner, CelebrationBanner. """
 import pygame
 
-# this base Banners class opens all banner types
+#THIS IS WHAT GETS THE GUI UP:
+pygame.init()
+screen = pygame.display.set_mode((800, 500))
+isRunning = True # bool for whether the program is running
+
+while isRunning:
+    for event in pygame.event.get():
+        # user must click "X" to exit program
+        if event.type == pygame.QUIT:
+            isRunning = False
+                
+    screen.fill("green")
+    font = pygame.font.Font(None, 36)
+
+    # Define start button
+    startButton = pygame.Rect(150, 300, 200, 80)
+    pygame.draw.rect(screen, "black", startButton)
+    startText = font.render("START", True, "white")
+    startTextRect = startText.get_rect(center=startButton.center)
+    screen.blit(startText, startTextRect)
+
+    # Define end button
+    endButton = pygame.Rect(450, 300, 200, 80)
+    pygame.draw.rect(screen, "black", endButton)
+    endText = font.render("END", True, "white")
+    endTextRect = endText.get_rect(center=endButton.center)
+    screen.blit(endText, endTextRect)
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # Detect a click on start+end buttons
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if startButton.collidepoint(event.pos):
+            print("Start button clicked")
+
+        elif endButton.collidepoint(event.pos):
+            print("End button clicked")
+
+pygame.quit()
+
+""" This base Banners class holds all banner types and forwards to the Banner that is actually called. """
 class Banners:
-    # starts game
-    pygame.init()
-    screen = pygame.display.set_mode((800, 500))
-    isRunning = True # bool for whether the program is running
+    def __init__(self, font):
+        # dictionary of screen options
+        self.screens = {"start": StartScreen(font),
+                        "play": RegularBanner(font),
+                        "celebration": CelebrationBanner(font),
+                        "end": EndScreen(font)}
+        self.active_name = "start"
 
-    while isRunning:
-        for event in pygame.event.get():
-            # user must click "X" to exit program
-            if event.type == pygame.QUIT:
-                isRunning = False   
-
-        screen.fill("green")
-
-        startButton = pygame.Rect(150, 300, 200, 80)
-        endButton = pygame.Rect(450, 300, 200, 80)
-
-        # list of buttons
-        buttList = [startButton, endButton]
-
-        pygame.draw.rect(screen, "black", startButton)
-        pygame.draw.rect(screen, "black", endButton)
-
-        # flip() the display to put your work on screen
-        pygame.display.flip()
-
-    pygame.quit()
+    startButton = pygame.Rect(150, 300, 200, 80)
+    endButton = pygame.Rect(450, 300, 200, 80)
+    
+    # list of buttons
+    buttList = [startButton, endButton]
 
 
 # StartScreen class:
 # - start/exit buttons to start/end program
 # - prompt user to pick team+rename team names; else keep default Team1+Team2 names
-class StartScreen():
+class StartScreen(Banners):
     def __init__(self, font, buttonType):
         self.font = font
+
+    pygame.draw.rect(screen, "black", startButton)
+    pygame.draw.rect(screen, "black", endButton)
+
+    # Detect a click
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if startButton.collidepoint(event.pos):
+            print("Start button clicked")
 
 # EndScreen class:
 # - stop all objects
 # - display final score
 # - option to restart match or end game (i.e. restart  or end program)
-class EndScreen():
+class EndScreen(Banners):
     def __init__(self, font):
         self.font = font
 
+    #pygame.draw.rect(screen, "black", restartButton)
+    pygame.draw.rect(screen, "black", endButton)
+
 # RegularBanner class:
 # - contains scoreboard, timer, and team names
-# - is a parent of Scoreboard and Timer, which are displayed during play
-class RegularBanner():
+class RegularBanner(Banners):
     def __init__(self, font):
         self.font = font
+
+    # TODO create scoreboard
+
+    # TODO create timer
+
+    # TODO box containing team names
 
 # CelebrationBanner class:
 # - displays when a team scores
 # - resets player and Ball layout(i.e. configuration at the start of the game)
-class CelebrationBanner():
+class CelebrationBanner(Banners):
     def __init__(self, font):
         self.font = font
